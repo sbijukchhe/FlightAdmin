30a44cfad914421d455463fc256d7a81c4e1e920
sbijukchhe <skbijukchhe@gmail.com>
2019-10-02 17:17:16 -0400

    Searching flight details

---
+++ src/main/java/com/example/demo/FlightRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface FlightRepository extends CrudRepository<Flight, Long> {
+    ArrayList<Flight> findByAirlineName(String airlineName);
+
+    void deleteByAirlineName(String airlineName);
+}
